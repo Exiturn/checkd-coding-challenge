4807c5304fdda7df30ae381c7e7034d73878855e
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-04 14:27:34 +0100

    Add new font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,20 @@
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
 export const metadta = {
   title: "Checkd Football Widget",
   description: "A simple widget for the Checkd frontend developer challenge"
 }
 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+})
+
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className}`}>
         <main className="w-screen h-screen flex justify-center items-center">
             {children}
         </main>
