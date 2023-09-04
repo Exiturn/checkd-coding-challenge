d9978ae02e6b983735afa9d8e8bae1e408a787bb
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-04 14:14:10 +0100

    Finished statGraphics component

--- components/StatGraphics.jsx
+++ components/StatGraphics.jsx
@@ -19,7 +19,52 @@ const StatGraphics = ({ homeStats, awayStats, isPercentage, statType }) => {
     }, [])
 
   return (
-    <div>StatGraphics</div>
+    <section className='w-full h-auto mt-6'>
+        <div className='flex justify-between items-center w-full h-auto mb-2'>
+            {/* Home Stat Value */}
+            {homeStats === undefined ? (
+                <p>Loading...</p>
+            ) : (
+                <p>
+                    {`${homeStats.value}`}
+                    {isPercentage ? "%" : "" }
+                </p>
+            )}
+
+            {/* Stat Category */}
+            {statType}
+
+            {/* Away Stat Value */}
+            {awayStats === undefined ? (
+                <p>Loading...</p>
+            ) : (
+                <p>
+                    {`${awayStats.value}`}
+                    {isPercentage ? "%" : "" }
+                </p>
+            )}
+        </div>
+
+        <div className='flex justify-center items-center mt-1 gap-x-2'>
+            {/* Home Stat Bar */}
+            <div className='bg-gray-400 w-[50%] h-[6px] rounded-full relative'>
+                {homeStats === undefined ? (
+                    <div />
+                ) : (
+                    <div className='absolute top-0 right-0 bg-green-600 h-[6px] rounded-full' style={{ width: `${homeValue}%` }} />
+                )}
+            </div>
+
+            {/* Away Stat Bar */}
+            <div className='bg-gray-400 w-[50%] h-[6px] rounded-xl relative'>
+                {awayStats === undefined ? (
+                    <div />
+                ) : (
+                    <div className='absolute top-0 left-0 bg-yellow-400 h-[6px] rounded-full' style={{ width: `${awayValue}%` }} />
+                )}
+            </div>
+        </div>
+    </section>
   )
 }
 
